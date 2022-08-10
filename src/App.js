48476defafb7367d3e59a6a360bd595768cfc9a7
debas-31 <debas31@gmail.com>
2022-08-11 00:08:59 +0300

    Add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import React from 'react'
+import { Provider } from 'react-redux';
+import {  BrowserRouter, Routes, Route } from 'react-router-dom'
+import Greeting from './Greeting';
+import Header from './Header';
+import Home from './Home';
+import store from '../redux/ConfigureStore';
 
 function App() {
   return (
     <div className="App">
-      
+    <Provider store={store}> 
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route exact path="/" element={ (<Home />) } />
+          <Route exact path="/greeting" element={ (<Greeting />) } />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
     </div>
   );
 }
